45bd8cbd8b2fa9b265c5a299053f7a99de97e136
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-08-05 20:08:28 -0400

    changed datepicker

--- src/components/TripForm.js
+++ src/components/TripForm.js
@@ -1,78 +1,128 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {addTrip} from '../actions/tripsAction'
-import {DateInput} from 'semantic-ui-calendar-react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { addTrip } from '../actions/tripsAction';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 class TripForm extends React.Component {
+	state = {
+		name: '',
+		water_type: '',
+		// start_date: '',
+		// end_date: ''
+		// start_date: '',
+		// end_date: ''
+		startDate: new Date()
+	};
 
-  state = {
-    name: '',
-    water_type: "",
-    start_date: '',
-    end_date: ''
-    // start_date: '',
-    // end_date: ''
-  }
+	handleChange = (date) => {
+		this.setState({
+			startDate: date
+		});
+	};
 
-  handleOnSubmit = (e) => {
-    e.preventDefault()
-    this.props.addTrip(this.state)
-    this.setState({name: '', water_type: ''})
-    this.props.history.push('/');
-  }
+	handleOnSubmit = (e) => {
+		e.preventDefault();
+		this.props.addTrip(this.state);
+		this.setState({ name: '', water_type: '' });
+		this.props.history.push('/');
+	};
 
-  handleOnChange = (e, {name, value}) => {
-    if (this.state.hasOwnProperty(name)) {
-      this.setState({[name]: value});
-    }
-  }
+	// handleOnChange = (e, { name, value }) => {
+	// 	if (this.state.hasOwnProperty(name)) {
+	// 		this.setState({ [name]: value });
+	// 	}
+	// };
 
-  handleIfChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
+	handleIfChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
 
-
-
-  render() {
-    // console.log(DateInput)
-    console.log('start_date', this.state.start_date)
-    return (<div>
-      <h4>add a trip!</h4>
-      <form onSubmit={this.handleOnSubmit}>
-        <div className="ui form">
-          <div className="fields">
-            <div className="five wide field">
-              <input onChange={this.handleIfChange} value={this.state.name}  type="text" name="name"   placeholder="name" autoComplete="off"/><br/>
-            </div>
-            <div className="three wide field">
-              <label>water type
-                <select onChange={this.handleIfChange} value={this.state.water_type} type="text"  name="water_type" placeholder="water type" autoComplete="off">
-                  <option value='select'>select below</option>
-                  <option value="bay">bay</option>
-                  <option value="lake">lake</option>
-                  <option value="ocean">ocean</option>
-                  <option value="river">river</option>
-                </select>
-              </label>
-            </div>
-            <div className="three wide field">
-              <DateInput onChange={this.handleOnChange} type={this.DateInput} dateFormat="MM-DD-YY" value={this.state.start_date} animation='off'  iconPosition="left" name="start_date" placeholder="start date" autoComplete="off"/>
-            </div>
-            <div className="three wide field">
-              <DateInput onChange={this.handleOnChange} type={this.DateInput} dateFormat="MM-DD-YY" value={this.state.end_date} animation='off' iconPosition="left"  name="end_date" placeholder="end date"   autoComplete="off"/>
-
-            </div>
-            <div className="two wide field">
-              <button type="submit" className="ui button">Submit</button>
-            </div>
-          </div>
-        </div>
-      </form>
-    </div>)
-  }
-
+	render() {
+		// console.log(DateInput)
+		console.log('start_date', this.state.start_date);
+		return (
+			<div>
+				<h4>add a trip!</h4>
+				<form onSubmit={this.handleOnSubmit}>
+					<div className="ui form">
+						<div className="fields">
+							<div className="five wide field">
+								<input
+									onChange={this.handleIfChange}
+									value={this.state.name}
+									type="text"
+									name="name"
+									placeholder="name"
+									autoComplete="off"
+								/>
+								<br />
+							</div>
+							<div className="three wide field">
+								<label>
+									water type
+									<select
+										onChange={this.handleIfChange}
+										value={this.state.water_type}
+										type="text"
+										name="water_type"
+										placeholder="water type"
+										autoComplete="off"
+									>
+										<option value="select">select below</option>
+										<option value="bay">bay</option>
+										<option value="lake">lake</option>
+										<option value="ocean">ocean</option>
+										<option value="river">river</option>
+									</select>
+								</label>
+							</div>
+							<div className="three wide field">
+								<DatePicker
+									selected={this.state.startDate}
+									onChange={this.handleChange}
+									autoComplete="off"
+								/>
+								{/* <DateInput
+									onChange={this.handleOnChange}
+									type={this.DateInput}
+									dateFormat="MM-DD-YY"
+									value={this.state.start_date}
+									animation="off"
+									iconPosition="left"
+									name="start_date"
+									placeholder="start date"
+									autoComplete="off"
+								/> */}
+							</div>
+							<div className="three wide field">
+								<DatePicker
+									selected={this.state.startDate}
+									onChange={this.handleChange}
+									autoComplete="off"
+									// name="start_date"
+									// placeholder="start date"
+								/>
+								{/* // <DateInput */}
+								{/* // onChange={this.handleOnChange}
+								// type={this.DateInput}
+								// dateFormat="MM-DD-YY" // value={this.state.end_date}
+								// animation="off" // iconPosition="left" // name="end_date" // placeholder="end date"
+								// autoComplete="off" // /> */}
+							</div>
+							<div className="two wide field">
+								<button type="submit" className="ui button">
+									Submit
+								</button>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+		);
+	}
 }
 
-export default connect(null, {addTrip, DateInput})(TripForm)
+export default connect(null, { addTrip, DatePicker })(TripForm);
