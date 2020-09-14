d522c147da2e33d147efd99984da464987b1cf7f
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-09-14 14:32:13 -0400

    update

--- src/containers/TripsContainer.js
+++ src/containers/TripsContainer.js
@@ -1,41 +1,31 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import Trips from '../components/Trips'
-import { deleteTrip } from '../actions/tripsAction'
-import { fetchTrips } from '../actions/tripsAction'
-
-
-
+import React from 'react';
+import { connect } from 'react-redux';
+import Trips from '../components/Trips';
+import { deleteTrip } from '../actions/tripsAction';
+import { fetchTrips } from '../actions/tripsAction';
 
 class TripsContainer extends React.Component {
+	componentDidMount() {
+		this.props.fetchTrips();
+	}
 
-  componentDidMount(){
-    this.props.fetchTrips()
-  }
+	componentDidUpdate(prevProps) {
+		if (this.props.trips.trips.length > prevProps.trips.trips.length) {
+			this.props.fetchTrips(this.props.trips.trips);
+		}
+	}
 
-  componentDidUpdate(prevProps) {
-    if (this.props.trips.trips.length > prevProps.trips.trips.length) {
-      this.props.fetchTrips(this.props.trips.trips);
-    }
-  }
-
-  
-
-
-
-
-
-  render () {
-    return (
-        <div>
-          <Trips trips={this.props.trips} deleteTrip={this.props.deleteTrip} />
-        </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<Trips trips={this.props.trips} deleteTrip={this.props.deleteTrip} />
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = state => {
-  return {trips: state.trips}
-}
+const mapStateToProps = (state) => {
+	return { trips: state.trips };
+};
 
-export default connect(mapStateToProps, {fetchTrips, deleteTrip})(TripsContainer)
+export default connect(mapStateToProps, { fetchTrips, deleteTrip })(TripsContainer);
