c8d01b9084e88ee84f2f55f5e0e7a037204b48ec
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2021-04-22 22:10:50 -0400

    added prettier

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import { Card, Icon } from 'semantic-ui-react';
+import React from 'react'
+import { Card, Icon } from 'semantic-ui-react'
 
 class Comment extends React.Component {
-	render() {
-		const { comment } = this.props;
+  render() {
+    const { comment } = this.props
 
-		return (
-			<div className="comment">
-				<Card.Content>
-					<h4>{comment.content}</h4>
-				</Card.Content>
+    return (
+      <div className="comment">
+        <Card.Content>
+          <h4>{comment.content}</h4>
+        </Card.Content>
 
-				<Card.Content extra>
-					<a>
-						<Icon name="delete" onClick={() => this.props.deleteComment(this.props.comment.id)} /> delete
-					</a>
-				</Card.Content>
-			</div>
-		);
-	}
+        <Card.Content extra>
+          <a>
+            <Icon
+              name="delete"
+              onClick={() => this.props.deleteComment(this.props.comment.id)}
+            />{' '}
+            delete
+          </a>
+        </Card.Content>
+      </div>
+    )
+  }
 }
 
-export default Comment;
+export default Comment
